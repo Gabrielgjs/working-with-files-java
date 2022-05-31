b93bb38fc42821dfe019a01c83e257ecc24840ab
gjsgabriel <gjsgabriel83@gmail.com>
2022-05-31 11:28:16 -0300

    csv implementação final

--- src/application/ProgramArquivos.java
+++ src/application/ProgramArquivos.java
@@ -1,10 +1,15 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 import entities.Product;
 
@@ -12,36 +17,53 @@ public class ProgramArquivos {
 
 	public static void main(String[] args) {
 		
-		String path = "C:\\temp\\in.txt";
-		
-		List<Product> list = new ArrayList<Product>();
-		
-		//Comando para abrir o arquivo que está armazenado na variável path.
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){
-			
-			String line = br.readLine();// lê 1 linha do arquivo!
-			line = br.readLine();
-			while(line != null) {
-				
-				String[] vect = line.split(","); // recorta variável line a partir da virgula, criando um vetor com os valores contidos em line.
-				String name = vect[0];
-				Double price = Double.parseDouble(vect[1]);
-				Integer  quantity = Integer.parseInt(vect[2]);
-				
-				Product prod = new Product(name,price,quantity);
-				list.add(prod);
-				
-				line = br.readLine();
-			}
-			System.out.println("PRODUCTS: ");
-			for (Product p : list) {
-				System.out.println(p);
-			}
-			
-		}
-		catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
-	}
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
 
+		List<Product> list = new ArrayList<>();
+
+		System.out.println("Enter file path: ");
+		String sourceFileStr = scan.nextLine();
+
+		File sourceFile = new File(sourceFileStr);
+		String sourceFolderStr = sourceFile.getParent();
+		
+		boolean success = new File(sourceFolderStr + "\\out").mkdir();
+		
+		String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
+
+			String itemCsv = br.readLine();
+			while (itemCsv != null) {
+
+				String[] fields = itemCsv.split(",");
+				String name = fields[0];
+				double price = Double.parseDouble(fields[1]);
+				int quantity = Integer.parseInt(fields[2]);
+
+				list.add(new Product(name, price, quantity));
+
+				itemCsv = br.readLine();
+			}
+
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
+
+				for (Product item : list) {
+					bw.write(item.getName() + "," + String.format("%.2f", item.total()));
+					bw.newLine();
+				}
+
+				System.out.println(targetFileStr + " CREATED!");
+				
+			} catch (IOException e) {
+				System.out.println("Error writing file: " + e.getMessage());
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
+		}
+
+		scan.close();
+	}
 }
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -42,12 +42,9 @@ public class Product implements Serializable{
 	public void setQuantity(Integer quantity) {
 		this.quantity = quantity;
 	}
-
-	@Override
-	public String toString() {
-		return "Product [name=" + name + ", price=" + price + ", quantity=" + quantity + "]";
+	
+	public double total() {
+		return price * quantity;
 	}
-	
-	
 	
 }
